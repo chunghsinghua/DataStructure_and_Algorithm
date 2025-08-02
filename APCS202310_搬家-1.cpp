e96ae4931016543d236529e48de8a3ab56d58b8f
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2025-08-02 20:23:56 +0800

    Update APCS202310_搬家-1.cpp

--- APCS202310_搬家-1.cpp
+++ APCS202310_搬家-1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define N 510
+#define N 50010
 typedef long long ll;
 
 bool east(char v)
@@ -21,55 +21,48 @@ bool south(char v)
 {
     return (v == 'X' || v == 'I' || v == '7' || v == 'F');
 }
-
-int visit[N][N];
-
 int main()
 {
-    // freopen("Q_7_8_2.in", "r", stdin);
     int m, n;
     cin >> m >> n;
-    string mat[m];
+    vector<string> mat(m);
     for (int i = 0; i < m; ++i)
         cin >> mat[i];
-    int mx = -1;
+    vector<vector<bool>> vis(m, vector<bool>(n, false));
+    int mx = 0;
     for (int i = 0; i < m; ++i)
     {
         for (int j = 0; j < n; ++j)
         {
-            if (visit[i][j] > 0 || mat[i][j] == '0')
+            if (vis[i][j] || mat[i][j] == '0')
                 continue;
             vector<pair<int, int>> que;
-            int head = 0;
-            visit[i][j] = 1;
             que.push_back({i, j});
+            vis[i][j] = true;
+            int head = 0;
             while (head < que.size())
             {
-                auto v = que[head++];
-                int r = v.first, c = v.second;
-                // east
-                if (c + 1 < n && east(mat[r][c]) && visit[r][c + 1] <= 0 && west(mat[r][c + 1]))
+                int r = que[head].first, c = que[head].second;
+                head++;
+                if (c + 1 < n && !vis[r][c + 1] && east(mat[r][c]) && west(mat[r][c + 1]))
                 {
+                    vis[r][c + 1] = true;
                     que.push_back({r, c + 1});
-                    visit[r][c + 1] = 1;
                 }
-                // west
-                if (c - 1 >= 0 && west(mat[r][c]) && visit[r][c - 1] <= 0 && east(mat[r][c - 1]))
+                if (c - 1 >= 0 && !vis[r][c - 1] && west(mat[r][c]) && east(mat[r][c - 1]))
                 {
+                    vis[r][c - 1] = true;
                     que.push_back({r, c - 1});
-                    visit[r][c - 1] = 1;
                 }
-                // south
-                if (r + 1 < m && south(mat[r][c]) && visit[r + 1][c] <= 0 && north(mat[r + 1][c]))
+                if (r + 1 < m && !vis[r + 1][c] && south(mat[r][c]) && north(mat[r + 1][c]))
                 {
+                    vis[r + 1][c] = true;
                     que.push_back({r + 1, c});
-                    visit[r + 1][c] = 1;
                 }
-                // north
-                if (r - 1 >= 0 && north(mat[r][c]) && visit[r - 1][c] <= 0 && south(mat[r - 1][c]))
+                if (r - 1 >= 0 && !vis[r - 1][c] && north(mat[r][c]) && south(mat[r - 1][c]))
                 {
+                    vis[r - 1][c] = true;
                     que.push_back({r - 1, c});
-                    visit[r - 1][c] = 1;
                 }
             }
             mx = max(mx, (int)que.size());
