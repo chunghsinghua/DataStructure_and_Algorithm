bcdf8a38fc0041fc9ea5269d7cbacff0fce01508
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2026-02-01 22:23:44 +0800

    Update APCS202401_合併成本(top-down).py

--- APCS202401_合併成本(top-down).py
+++ APCS202401_合併成本(top-down).py
@@ -1,19 +1,20 @@
-def dp(le,ri):
-    if dpn[le][ri]>=0:
-        return dpn[le][ri]
-    total = sum(a[le:ri+1])
+def cost(le, ri):
+    if dp[le][ri]>=0:
+        return dp[le][ri]
+    tot = sum(vec[le:ri+1])
+    imin = 1001*100
     lsum = 0
-    mn = 1e9    
-    for i in range(le,ri):
-        lsum += a[i]
-        mn = min(mn,dp(le,i)+dp(i+1,ri)+abs(total-lsum-lsum))
-    dpn[le][ri]= mn
-    return dpn[le][ri]
-    
+    for i in range(le, ri):
+        lsum += vec[i]
+        tmp = cost(le,i) + cost(i+1,ri)+abs(tot-lsum-lsum)
+        imin = min(tmp,imin)
+    dp[le][ri] = imin
+    return dp[le][ri]
 
-n=int(input())
-dpn=[[-1]*(n+1) for i in range (n+1)]
-a=[0]+[int(x) for x in input().split()]
-for i in range (n+1):
-    dpn[i][i]=0
-print(dp(1,n))
+
+n =int(input())
+vec = [int(x) for x in input().split()]
+dp = [[-1]*n for _ in range(n)]
+for i in range(n):
+    dp[i][i]=0
+print(cost(0,n-1))
