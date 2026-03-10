38107d7ca59147ec477cb274ba4f512dadd65c9e
chunghsinghua <63764300+chunghsinghua@users.noreply.github.com>
2026-03-10 15:25:38 +0800

    Fix binary search condition to include right index

--- Algorithm_BinarySearch(like lower_bound).cpp
+++ Algorithm_BinarySearch(like lower_bound).cpp
@@ -1,9 +1,10 @@
 // find the first >= x between a[left..right]
 int bsearch(int a[], int left, int right, int x)
 {
-    while (left < right)
+    while (left <= right)
     {
         int mid = (left + right) >> 1;
+        if (a[mid]==x) return mid;
         if (a[mid] < x)
             left = mid + 1;
         else
